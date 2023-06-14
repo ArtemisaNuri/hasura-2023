a0c565df1ab2f5d99146f4e5a992703fcc240050
ArtemisaNuri <135178115+ArtemisaNuri@users.noreply.github.com>
2023-06-14 09:03:50 +0000

    students

---
+++ hasura-badges/migrations/default/1686728488387_test-migration/up.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE IF NOT EXISTS students (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  age INTEGER,
+  grade VARCHAR(10)
+);
---
+++ hasura-badges/seeds/default/studentSeed.sql
@@ -0,0 +1,8 @@
+INSERT INTO students (name, age, grade)
+VALUES
+  ('John Smith', 18, 'A'),
+  ('Jane Doe', 17, 'B'),
+  ('Michael Johnson', 19, 'A-'),
+  ('Emily Davis', 16, 'B+');
+
+ 
