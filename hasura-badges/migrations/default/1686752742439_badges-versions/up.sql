bfa01832914df24dd57b50fcc2362b473d461bee
ArtemisaNuri <135178115+ArtemisaNuri@users.noreply.github.com>
2023-06-15 09:56:03 +0000

    tenant id chnges

--- hasura-badges/migrations/default/1686752742439_badges-versions/up.sql
+++ hasura-badges/migrations/default/1686752742439_badges-versions/up.sql
@@ -26,29 +26,8 @@ BEGIN
     "created_at",
     "created_by"
   )
-  SELECT 
-    "bd"."id", 
-    "bd"."title", 
-    "bd"."description", 
-    coalesce(
-        json_agg(
-            json_build_object(
-                'id', "rd"."id", 
-                'title', "rd"."title", 
-                'description', "rd"."description"
-            ) 
-            ORDER BY "rd"."id"
-        ) FILTER (WHERE "rd"."id" IS NOT NULL),
-        '[]'::json
-    ) AS "requirements",
-    version_at, 
-    user_id
-  FROM "badges_definitions" "bd"
-  LEFT JOIN "requirements_definitions" "rd" ON "bd"."id" = "rd"."badge_id"
-  WHERE "bd"."id" = "badge_def_id"
-  GROUP BY "bd"."id", "bd"."title", "bd"."description"
-  RETURNING *;
-END; $$ LANGUAGE plpgsql;
+  
+  
 
 -- Public version of the function
 -- (will receive the Hasura Session to figure out the user)
@@ -58,7 +37,7 @@ CREATE OR REPLACE FUNCTION "create_badge_version"(
 )
 RETURNS SETOF "badges_versions" AS $$
 DECLARE
-  user_id integer := (hasura_session ->> 'x-hasura-user-id')::integer;
+  tenant_id integer := (hasura_session ->> 'x-hasura-tenant-id')::integer;
 BEGIN
   RETURN QUERY
   SELECT * FROM _create_badge_version(user_id, badge_def_id, (SELECT now() AT TIME ZONE 'UTC'));
